8176d369d6a1ecdef8bf7bc6968051355af203ce
yannsun <sunjinliang1992@gmail.com>
2018-04-03 21:48:52 +0800

    update

--- user/tag.go
+++ user/tag.go
@@ -10,15 +10,17 @@ const (
 	GetTagsUrl string = "https://api.weixin.qq.com/cgi-bin/tags/get"
 )
 
+type Tag struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Count string `json:"count"`
+}
 // 创建用户标签
 type TagName struct {
 	Name string `json:"name"`
 }
 type CreateTagReq struct {
 	Tag TagName `json:"tag"`
-}
-type TagID struct {
-	ID string `json:"id"`
 }
 type CreateTagRes struct {
 	common.CommonResponse
@@ -30,15 +32,34 @@ func CreateTag(client httper.HttpClient, request *CreateTagReq) (*CreateTagRes, 
 }
 
 // 获取公众号已创建的标签
-type Tag struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Count string `json:"count"`
-}
 type GetTagsRes struct {
 	Tags []Tag `json:"tags"`
 }
 func GetTags(client httper.HttpClient) (*GetTagsRes, error) {
 	var response = &GetTagsRes{}
+	return response, nil
+}
+
+// 编辑标签
+type UpdateTagReq struct {
+
+}
+type UpdateTagRes struct {
+	common.CommonResponse
+}
+func UpdateTag(client httper.HttpClient, request *UpdateTagReq) (*UpdateTagRes, error) {
+	var response = &UpdateTagRes{}
+	return response, nil
+}
+
+// 删除标签
+type DeleteTagReq struct {
+
+}
+type DeleteTagRes struct {
+
+}
+func DeleteTag(client httper.HttpClient, request *DeleteTagReq) (*DeleteTagRes, error) {
+	var response = &DeleteTagRes{}
 	return response, nil
 }
